66260d4a101fc8b92fa940fe7c79c701ceeeb208
Alexman2505 <alexman2505@gmail.com>
2026-02-03 16:03:24 +0300

    разбираемся с сокетами 2

--- simple_test2_client.py
+++ simple_test2_client.py
@@ -1,13 +1,12 @@
 import socket
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 8686))
-sock.send('hello, world!'.encode('utf-8'))  # или b'hello, world!'
-data = sock.recv(1024)
-if data:
-    print(f"Received: {data.decode('utf-8')}")
-else:
-    print("No data received")
-sock.close()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 8686))
+client_socket.send(
+    'Hello from client'.encode('utf-8')
+)  # или b'Hello' Это 5 байт: [72, 101, 108, 108, 111]
+data = client_socket.recv(1024)
+print(f"Received: {data.decode('utf-8')}")
+client_socket.close()
 print("Connection closed")
--- simple_test_server.py
+++ simple_test_server.py
@@ -1,11 +1,33 @@
 import socket
 
+# Сокет — это программный объект (интерфейс), представляющий конечную точку сетевого соединения,
+# определяемую уникальной комбинацией IP-адреса, номера порта и транспортного протокола,
+# предоставляющий интерфейс для отправки и получения данных через сеть.
 
-# Задаем адрес сервера
+# Задаем адрес сервера (комбинация 4 элементов):
+# IP-адрес (куда/откуда)
+# Номер порта (какому приложению)
+# Транспортный протокол (TCP или UDP)
+# Состояние соединения (если TCP)
+
+# Приложение
+#     ↓
+# Библиотека сокетов (socket API)
+#     ↓
+# Транспортный уровень (TCP/UDP) ←→ Буферы ОС
+#     ↓
+# Сетевой уровень (IP)
+#     ↓
+# Канальный уровень (Ethernet/WiFi)
+#     ↓
+# Физический уровень (провода/радио)
+
+
+server_socket = socket.socket(
+    socket.AF_INET,  # IPv4 протокол (семейство адресов)
+    socket.SOCK_STREAM,  # TCP протокол (тип сокета)
+)
 SERVER_ADDRESS = ('localhost', 8686)
-
-# Настраиваем сокет
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(SERVER_ADDRESS)
 server_socket.listen(10)
 print('server is running, please, press ctrl+c to stop')
@@ -13,6 +35,7 @@ print('server is running, please, press ctrl+c to stop')
 # Слушаем запросы
 while True:
     connection, address = server_socket.accept()
+    print()
     print("new connection from {address}".format(address=address))
     data = connection.recv(1024)
     print(str(data))
