8c351cb8cb70c0a756bb2ca481e8949862a77618
Alexman2505 <alexman2505@gmail.com>
2026-02-05 13:57:27 +0300

    для истории

--- simple_client.py
+++ simple_client.py
@@ -1,3 +1,34 @@
+# import socket
+
+
+# def start_tcp_client():
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect(('localhost', 12345))  # Подключаемся к серверу
+
+#     message = "Привет, сервер!".encode()  # Преобразуем строку в байты
+#     client_socket.sendall(message)  # Отправляем сообщение
+
+#     response = client_socket.recv(1024)  # Получаем ответ
+#     print(f"Ответ сервера: {response.decode()}")
+
+
+#     client_socket.close()  # Закрываем соединение
+
+
+# def start_udp_client():
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#     message = "Привет, UDP-сервер!".encode()
+#     sock.sendto(message, ('localhost', 12345))  # Отправляем сообщение
+
+#     response, _ = sock.recvfrom(1024)  # Получаем ответ
+#     print(f"Ответ сервера: {response.decode()}")
+
+#     sock.close()
+
+
+# start_udp_client()
+
 import socket
 import time
 import json
--- simple_server.py
+++ simple_server.py
@@ -1,3 +1,41 @@
+# import socket
+
+
+# def start_tcp_server():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(('localhost', 12345))  # Привязываем к адресу и порту
+#     server_socket.listen(1)  # Начинаем слушать (1 - макс. очередь подключений)
+
+#     print("Сервер запущен и ожидает подключений...")
+
+#     while True:
+#         client_socket, addr = server_socket.accept()  # Принимаем подключение
+#         print(f"Подключен клиент: {addr}")
+
+#         data = client_socket.recv(1024)  # Получаем данные (макс. 1024 байт)
+#         print(f"Получено: {data.decode()}")
+
+#         client_socket.sendall(data)  # Отправляем данные обратно
+#         client_socket.close()  # Закрываем соединение
+
+
+# start_tcp_server()
+
+
+# def start_udp_server():
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     sock.bind(('localhost', 12345))
+
+#     print("UDP-сервер запущен...")
+
+#     while True:
+#         data, addr = sock.recvfrom(1024)  # Получаем данные и адрес отправителя
+#         print(f"Получено от {addr}: {data.decode()}")
+#         sock.sendto(data, addr)  # Отправляем ответ обратно
+
+
+# start_udp_server()
+
 import socket
 import json
 
