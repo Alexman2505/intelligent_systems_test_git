a00be5b61ec1ae140b67516f2755fd68414e5465
Alexman2505 <alexman2505@gmail.com>
2026-02-13 01:33:10 +0300

    ну вот я и разобрался как же джанго/фласк парсит входящий http запрос из сокета и превращает его в запрос, который маршрутизируется уже внутри приложения. конечно же еще нужен гуникорн, который сам создает из hhtp запроса словарь для парсинга внутри джанги. молчанов

--- for_history/simple_server.py
+++ for_history/simple_server.py
@@ -1,25 +1,79 @@
 import socket
 
+URLS = {'/': 'hello index', '/blog': 'hello blog'}
+
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+
+def generate_headers(method, url):
+    if not method == 'GET':
+        return ('HTTP/1.1 405 Method not allowed\n', 405)
+
+    if not url in URLS:
+        return ('HTTP/1.1 404 Method not found\n', 404)
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowed</p>'
+    return '<h1>{}</h1>'.format(URLS[url])
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+    return (headers + body).encode(encoding='utf-8')
+
 
 def start_tcp_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost', 12345))  # Привязываем к адресу и порту
     server_socket.listen(1)  # Начинаем слушать (1 - макс. очередь подключений)
-
     print("Сервер запущен и ожидает подключений...")
 
     while True:
         client_socket, addr = server_socket.accept()  # Принимаем подключение
         print(f"Подключен клиент: {addr}")
 
-        data = client_socket.recv(1024)  # Получаем данные (макс. 1024 байт)
-        print(f"Получено: {data.decode()}")
+        request = client_socket.recv(1024)  # Получаем данные (макс. 1024 байт)
+        print(f"Получено: {request.decode(encoding='utf-8')}")
 
-        client_socket.sendall(data)  # Отправляем данные обратно
+        if not request:
+            print(
+                '-----------------------------------------------------------------'
+            )
+            print("Получен пустой запрос, закрываем соединение")
+            print(
+                '-----------------------------------------------------------------'
+            )
+            client_socket.close()
+            continue  # Пропускаем итерацию
+
+        response = generate_response(request.decode(encoding='utf-8'))
+        client_socket.sendall(response)  # Отправляем данные обратно
+        print('-------------------')
+        print(f"Это ответ response {response}")
+        print(
+            '-----------------------------------------------------------------'
+        )
+        print()
+        print()
+        print()
+        print()
         client_socket.close()  # Закрываем соединение
 
 
-# start_tcp_server()
+if __name__ == '__main__':
+    start_tcp_server()
 
 ######################################
 # def start_udp_server():
